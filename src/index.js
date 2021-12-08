8b59a705c3d186af781a0711e3757589ff88bf65
micheaol <micheaol80@gmail.com>
2021-12-08 13:41:50 +0100

    remove disable linter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import store from '../redux/configureStore';
+import store from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
